3be93b787e13d753ebe3e5d3c58ee615cf80200a
AugustoKark <augustokark@hotmail.com>
2024-10-04 09:41:10 -0300

    Initial version of techmarket generated by generator-jhipster@8.7.1

---
+++ src/test/java/edu/um/alumno/TechnicalStructureTest.java
@@ -0,0 +1,38 @@
+package edu.um.alumno;
+
+import static com.tngtech.archunit.base.DescribedPredicate.alwaysTrue;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.belongToAnyOf;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packagesOf = TechmarketApp.class, importOptions = DoNotIncludeTests.class)
+class TechnicalStructureTest {
+
+    // prettier-ignore
+    @ArchTest
+    static final ArchRule respectsTechnicalArchitectureLayers = layeredArchitecture()
+        .consideringAllDependencies()
+        .layer("Config").definedBy("..config..")
+        .layer("Web").definedBy("..web..")
+        .optionalLayer("Service").definedBy("..service..")
+        .layer("Security").definedBy("..security..")
+        .optionalLayer("Persistence").definedBy("..repository..")
+        .layer("Domain").definedBy("..domain..")
+
+        .whereLayer("Config").mayNotBeAccessedByAnyLayer()
+        .whereLayer("Web").mayOnlyBeAccessedByLayers("Config")
+        .whereLayer("Service").mayOnlyBeAccessedByLayers("Web", "Config")
+        .whereLayer("Security").mayOnlyBeAccessedByLayers("Config", "Service", "Web")
+        .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Service", "Security", "Web", "Config")
+        .whereLayer("Domain").mayOnlyBeAccessedByLayers("Persistence", "Service", "Security", "Web", "Config")
+
+        .ignoreDependency(belongToAnyOf(TechmarketApp.class), alwaysTrue())
+        .ignoreDependency(alwaysTrue(), belongToAnyOf(
+            edu.um.alumno.config.Constants.class,
+            edu.um.alumno.config.ApplicationProperties.class
+        ));
+}
